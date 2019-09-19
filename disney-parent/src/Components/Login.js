9b2d69f83817c6221afc84fb595531bf44ac3925
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-09-19 18:35:55 -0400

    adding styling to the login/signup page

--- disney-parent/src/Components/Login.js
+++ disney-parent/src/Components/Login.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Form, Field, withFormik } from 'formik';
 import * as yup from 'yup';
 import styled from 'styled-components';
+import { primary1 } from './Styles';
+
 
 
 const Login = ({ errors, touched, status }) => {
@@ -15,18 +17,31 @@ const Login = ({ errors, touched, status }) => {
         font-size: 3rem;
         font-weight: 300;
     `
+    const SubmitButton = styled.button`
+        padding: 10px 80px;
+        text-align: center;
+        align-self: flex-start;
+        background: ${primary1};
+        color: white;
+        font-size: 1.2rem;
+        margin-top: 10px;
+    `
 
     return (
         <Wrapper>
             <Title>Login</Title>
-            <Form>
+            <Form style={{
+                display: 'flex',
+                flexFlow: 'Column Wrap',
+                alignContent: 'center',
+                }}>
                 {touched.email && errors.email && <p className='error'>{errors.email}</p>}
-                <Field type='email' name='email' placeholder='Email' />
+                <Field type='email' name='email' placeholder='Email' style={{alignSelf: 'flex-start'}} />
 
                 {touched.password && errors.password && <p className='error'>{errors.password}</p>}
-                <Field type='password' name='password' placeholder='Password' />
+                <Field type='password' name='password' placeholder='Password' style={{alignSelf: 'flex-start'}} />
 
-                <button type='submit'>Submit</button>
+                <SubmitButton type='submit' style={{alignSelf: 'flex-start'}}>Submit</SubmitButton>
             </Form>
         </Wrapper>
         
--- disney-parent/src/Components/Welcome.js
+++ disney-parent/src/Components/Welcome.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
 const Title = styled.h1`
     font-size: 4rem;
     font-weight: 300;
     text-align: center;
+    
 `
 
 
