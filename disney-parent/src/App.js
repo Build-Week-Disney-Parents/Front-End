544a92f08065d206b9b9f89c0dca82bce7dd5de6
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-09-19 17:47:19 -0400

    got rid of the semantic ui tab nav idea for the login/signup page, going with a different approach

--- disney-parent/src/App.js
+++ disney-parent/src/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
 import Welcome from './Components/Welcome';
-import AuthTabNav from './Components/Tab/AuthTabNav';
 import { Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <main>
-      <AuthTabNav />
-      <Route path='/' component={Welcome} />
+      <Welcome />
+      <Route exact path='/' component={Login} />
       <Route path='/signup' component={Signup} />
-      <Route path='/login' component={Login} />
     </main>
   )
 }
