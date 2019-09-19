8af4ff4c94fad9346911caa62f68f978650d3e58
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-09-18 20:39:06 -0400

    added a basic skeleton with formik for sign up and login, going to work with routing and semantic ui to get some routing functionality

--- disney-parent/src/App.js
+++ disney-parent/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Login from './Components/Login';
+import Signup from './Components/Signup';
 
 
 function App() {
   return (
     <div>
-      <Login />
+      {/* <Signup />
+      <Login /> */}
     </div>
   )
 }
--- disney-parent/src/Components/Login.js
+++ disney-parent/src/Components/Login.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Field, withFormik } from 'formik';
 import * as yup from 'yup';
+import styled from 'styled-components';
 
-const Login = () => {
+
+const Login = ({ errors, touched, status }) => {
+    const [values, setValues] = useState([]);
+
+    const Wrapper = styled.div`
+        width: 100%;
+        text-align: center;
+    `
+
     return (
-        <div>
+        <Wrapper>
+            <h1>Login</h1>
+            <Form>
+                {touched.email && errors.email && <p className='error'>{errors.email}</p>}
+                <Field type='email' name='email' placeholder='Email' />
 
-        </div>
+                {touched.password && errors.password && <p className='error'>{errors.password}</p>}
+                <Field type='password' name='password' placeholder='Password' />
+
+                <button type='submit'>Submit</button>
+            </Form>
+        </Wrapper>
+        
     )
 }
 
 export default withFormik({
-    handleSubmit: (values, { setSubmitting }) => {
-        console.log('submitted Email:', values.email)
-        console.log('submitted password:', values.password)
-        setTimeout(() => setSubmitting(false, 3 * 1000))
+    mapPropsToValues: (values) => {
+        return {
+            email: values.email || '',
+            password: values.password || ''
+        }
     },
-    validationSchema: LoginValidation,
+    validationSchema: yup.object().shape({
+        email: yup.string().required('Email is required!'),
+        password: yup.string().required('Password is required!')
+    }),
+    handleSubmit: (values, { setStatus }) => {
+        console.log('Email is', values.email)
+        console.log('Password is', values.password)
+    }
+    
 })(Login);
