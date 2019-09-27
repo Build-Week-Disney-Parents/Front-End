01a20f1b769711e25b0904c705c8cf09a0e262aa
Leon Chisum <leonchisum3306@gmail.com>
2019-09-27 06:42:18 -0400

    fixed routing, now shows only 1 UserCard when clicked!

--- disney-parent/src/Components/Dashboard/Queue/UserCard/Messages.jsx
+++ disney-parent/src/Components/Dashboard/Queue/UserCard/Messages.jsx
@@ -4,6 +4,7 @@ function Messages(props) {
     return (
         <div>
             <p>{props.chat.message}</p>
+            <button onClick={props.delMessage}>Click to Delete</button>
         </div>
     )
 }
